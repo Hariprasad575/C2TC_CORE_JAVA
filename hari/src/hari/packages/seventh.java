18f5b6fa630cd747036d7848e6d5e2682d224bef
Windows 10 <Windows 10@DESKTOP-MUFFQ4C>
2022-10-19 10:52:09 +0530

    arithmetic operators

---
+++ hari/src/hari/packages/seventh.java
@@ -0,0 +1,25 @@
+package hari.packages;
+
+public class seventh {
+
+	public static void main(String[] args) {
+	    
+	    
+	    int a = 12, b = 5;
+
+	    
+	    System.out.println("a + b = " + (a + b));
+
+	    
+	    System.out.println("a - b = " + (a - b));
+
+	    
+	    System.out.println("a * b = " + (a * b));
+
+	    
+	    System.out.println("a / b = " + (a / b));
+
+	   
+	    System.out.println("a % b = " + (a % b));
+	  }
+	}
