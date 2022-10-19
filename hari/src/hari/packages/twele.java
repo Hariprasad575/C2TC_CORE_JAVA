299de8891fad3f1a422b98e10623205232538ff9
Windows 10 <Windows 10@DESKTOP-MUFFQ4C>
2022-10-19 11:18:17 +0530

    logical operators

---
+++ hari/src/hari/packages/twele.java
@@ -0,0 +1,19 @@
+package hari.packages;
+
+public class twele {
+
+	public static void main(String[] args){
+
+	  
+	    System.out.println((5 > 3) && (8 > 5));  
+	    System.out.println((5 > 3) && (8 < 5)); 
+	    
+	    System.out.println((5 < 3) || (8 > 5));  
+	    System.out.println((5 > 3) || (8 < 5));  
+	    System.out.println((5 < 3) || (8 < 5));  
+
+	   
+	    System.out.println(!(5 == 3));
+	    System.out.println(!(5 > 3));  
+	  }
+	}
