558709c3e3270c18852f1bb807c0239f6ea0532b
Windows 10 <Windows 10@DESKTOP-MUFFQ4C>
2022-10-19 11:11:38 +0530

    assignment operators

---
+++ hari/src/hari/packages/ten.java
@@ -0,0 +1,17 @@
+package hari.packages;
+
+public class ten {
+
+	public static void main(String[] args) {
+	    
+	    
+	    int a = 4;
+	    int var; 
+	    var = a;
+	    System.out.println("var using =: " + var);
+	    var += a;
+	    System.out.println("var using +=: " + var);
+	    var *= a;
+	    System.out.println("var using *=: " + var);
+	  }
+	}
