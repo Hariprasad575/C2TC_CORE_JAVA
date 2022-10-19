30128daab310f1548106e0efdda6b9829da1dc61
Windows 10 <Windows 10@DESKTOP-MUFFQ4C>
2022-10-19 11:13:52 +0530

    increment and decrement operators

---
+++ hari/src/hari/packages/eleven.java
@@ -0,0 +1,25 @@
+package hari.packages;
+
+public class eleven {
+
+	public static void main(String[] args){
+	    
+	    
+	    int a = 12, b = 12;
+	    int result1, result2;
+
+	   
+	    System.out.println("Value of a: " + a);
+
+	    
+	    result1 = ++a;
+	    System.out.println("After increment: " + result1);
+
+	    System.out.println("Value of b: " + b);
+
+	    
+	    result2 = --b;
+	    System.out.println("After decrement: " + result2);
+	  }
+	}
+
