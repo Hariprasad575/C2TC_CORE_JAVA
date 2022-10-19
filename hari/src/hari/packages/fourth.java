d8e2d5627507eec1a41eef3aeaa69a23b53c1440
Windows 10 <Windows 10@DESKTOP-MUFFQ4C>
2022-10-19 10:46:49 +0530

    relational operators

---
+++ hari/src/hari/packages/fourth.java
@@ -0,0 +1,25 @@
+package hari.packages;
+
+public class fourth {
+
+	public static void main(String[] args){
+	    
+	   
+	    int a = 7, b = 11;
+
+	    
+	    System.out.println("a is " + a + " and b is " + b);
+
+	    System.out.println(a == b);  
+
+	    
+	    System.out.println(a != b); 
+	 
+	    System.out.println(a > b);  
+	    
+	    System.out.println(a < b); 
+	   
+	    System.out.println(a >= b);
+       System.out.println(a <= b);   
+	}
+	}
