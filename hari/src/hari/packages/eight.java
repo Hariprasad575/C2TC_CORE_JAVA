bfaa7dff7444f7d6045d974f2f2b891768794c87
Windows 10 <Windows 10@DESKTOP-MUFFQ4C>
2022-10-19 11:02:24 +0530

    continue statement

---
+++ hari/src/hari/packages/eight.java
@@ -0,0 +1,17 @@
+package hari.packages;
+
+public class eight {
+
+	public static void main(String[] args) {
+
+	    
+	    for (int i = 1; i <= 10; ++i) {
+
+	      
+	      if (i > 4 && i < 9) {
+	        continue;
+	      }
+	      System.out.println(i);
+	    }
+	  }
+	}
