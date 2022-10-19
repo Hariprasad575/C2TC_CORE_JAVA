3f9fbed1929c3aba1a4fcdef416b1e6bb5786aad
Windows 10 <Windows 10@DESKTOP-MUFFQ4C>
2022-10-19 11:08:54 +0530

    switch case

---
+++ hari/src/hari/packages/nine.java
@@ -0,0 +1,25 @@
+package hari.packages;
+
+public class nine {
+
+	public static void main(String[] args){
+
+	    int expression = 2;
+
+	    // switch statement to check size
+	    switch (expression) {
+	      case 1:
+	        System.out.println("Case 1");
+
+	        // matching case
+	      case 2:
+	        System.out.println("Case 2");
+
+	      case 3:
+	        System.out.println("Case 3");
+
+	      default:
+	        System.out.println("Default case");
+	    }
+	  }
+	}
