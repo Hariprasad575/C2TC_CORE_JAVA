17c047e14fc7c123d3f9c44ec6bc246a6910c0e0
Windows 10 <Windows 10@DESKTOP-MUFFQ4C>
2022-10-19 11:19:41 +0530

    while loop

---
+++ hari/src/hari/packages/thirteen.java
@@ -0,0 +1,16 @@
+package hari.packages;
+
+public class thirteen {
+
+	public static void main(String[] args) {
+
+	   
+	    int i = 1, n = 5;
+
+	   
+	    while(i <= n) {
+	      System.out.println(i);
+	      i++;
+	    }
+	  }
+	}
